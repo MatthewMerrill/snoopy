d22a85e2e9b984c9ee88074dfa9cc56f17322f31
Matthew Merrill <mattmerr47@gmail.com>
2020-07-15 21:13:45 -0700

    Last-Modified: Sat, 11 Jul 2020 00:08:28 GMT

---
+++ stores/download_snapshot.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+if [ $# -ne 1 ]; then
+  echo "$0 site.example";
+fi
+
+wget --execute="robots = off" --mirror --convert-links --no-parent --wait=.1 \
+  "$1"
+
+for file in $(find "$1" | grep -E "\.html$"); do
+ sed -i -E "s/data-cfemail=\"(.+)\"/data-cfemail=\"CLOUDFLARE_PROTECTED_EMAIL\"/" \
+   "$file"
+done
+
+yarn run prettier --write "stores/$1"
+
+git add .
+git commit -m "$(curl -ILs "$1" | grep Last-Modified)"
